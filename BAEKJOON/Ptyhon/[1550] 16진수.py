05d2a440bb4d32073b45ab28ceb27bdc20586c78
jeongtai <zzze37@naver.com>
2022-04-16 03:11:46 +0900

    백준 1550 16진수

--- BAEKJOON/Ptyhon/[1550] 16진수.py
+++ BAEKJOON/Ptyhon/[1550] 16진수.py
@@ -1,1 +1,24 @@
-print(int(input(), 16))
+import sys
+
+S = 0b0
+
+for _ in range(int(sys.stdin.readline())):
+    commends = list(sys.stdin.readline().split())
+
+    if commends[0] == 'add':
+        S = S | (0b1 << int(commends[1]))
+    elif commends[0] == 'remove':
+        if (S & (0b1 << int(commends[1]))):
+            S = S & ~(0b1 << int(commends[1]))
+    elif commends[0] == 'check':
+        if (S & (0b1 << int(commends[1]))):
+            print(1)
+        else:
+            print(0)
+    elif commends[0] == 'toggle':
+        S = S ^ (0b1 << int(commends[1]))
+            
+    elif commends[0] == 'all':
+        S = 0b111111111111111111111
+    elif commends[0] == 'empty':
+        S = 0b0
